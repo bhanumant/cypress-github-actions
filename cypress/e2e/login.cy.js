64c5e0e2d24ea8f2c3dcf35a24e912300449144b
bhanumant <hanumantbhojane1995@gmail.com>
2026-03-06 09:50:13 +0530

    Added Cypress CI

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+import credetials from "../fixtures/example.json";
+
+describe("Test cases for login", () => {
+  it.skip("login with valid credentials", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.get("input[name='username']").type(credetials.username);
+    cy.get("input[name='password']").type(credetials.password);
+    cy.get("button[type='submit']").click();
+    cy.title().should("eq", "OrangeHRM");
+    
+
+    // #Id
+    // .class
+    // input[tagname]
+    // xpath
+  });
+
+  it("login with valid credentials with custom command", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.login(credetials.username, credetials.password);
+    cy.title().should("eq", "OrangeHRM");
+
+    // #Id
+    // .class
+    // input[tagname]
+    // xpath
+  });
+
+  it("login with baseurl config", () => {
+    cy.visit("/web/index.php/auth/login");
+    cy.login(credetials.username, credetials.password);
+    cy.title().should("eq", "OrangeHRM");
+
+    // #Id
+    // .class
+    // input[tagname]
+    // xpath
+  });
+});
